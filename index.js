c2b56f28b411c83898cf1fd37bccb7f8f53717e4
Mohamedaboda-bit <mohamedabpd477@gmail.com>
2025-05-17 12:33:01 +0300

    edited pakage.json

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import { PrismaClient } from '@prisma/client';
+
+
+const app = express();
 
 app.use(cors({
     origin: '*', 
 }));
 
 dotenv.config();
-const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase';
 
@@ -17,7 +19,4 @@ app.use(express.json());
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+
